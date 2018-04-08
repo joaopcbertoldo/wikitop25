8c8c2137ea90ff0a1f9939d7a82abd9ad39fe3b4
Joao P C Bertoldo <joaopcbertoldo@gmail.com>
2018-04-08 20:58:14 +0200

    extract content from the gz file

--- src/compute_rank.py
+++ src/compute_rank.py
@@ -1,6 +1,26 @@
+import gzip
+from typing import List
 from datetime import datetime
+
 from src.download import download_pageviews
 
-dt = datetime(year=2017, month=3, day=1, hour=0)
-fpath = download_pageviews(dt)
+
+def _transform(line: bytes) -> List:
+    return line.decode('utf-8').split()[0:3]
+
+
+def extract_content(dt: datetime) -> List:
+
+    fpath = download_pageviews(dt)
+
+    with gzip.open(fpath, 'rb') as f:
+        content = [_transform(line) for line in f.readlines()]
+
+    return content
+
+
+if __name__ == '__main__':
+
+    dt = datetime(year=2017, month=3, day=1, hour=0)
+    c = extract_content(dt)
 
